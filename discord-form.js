5a14b9147c3c3ad3bb0c37f66c2ff9eb38cfa34a
ABHINAV TOMAR <atomar.cse18@chitkarauniversity.edu.in>
2020-08-11 20:00:43 +0530

    Inital Commit 2

--- discord-form.js
+++ discord-form.js
@@ -1,14 +1,12 @@
 const Discord = require('discord.js');
-const { site_owner, token, buttonId } = require('./config.json');
+const { site_owner, token, buttonId, formClass } = require('./config.json');
 
 const client = new Discord.Client();
-
-const btn = document.getElementById(buttonId);
 
 client.on('ready', () => {
     client.user.setActivity("staring into void of existence.", { type: "PLAYING"});
 });
 
-
+console.log('Called');
 
 client.login(token);
