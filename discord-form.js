b240fa2d420747472ac6782e4cdf2d4bbcf97272
ABHINAV TOMAR <atomar.cse18@chitkarauniversity.edu.in>
2020-08-13 22:23:40 +0530

    Fixed Discord Message Send Process

--- discord-form.js
+++ discord-form.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
-const { site_owner, token, buttonId, formClass } = require('./config.json');
+const { MessageEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+
+const { site_owner, token, buttonId, formPage } = require('./config.json');
 
 const client = new Discord.Client();
 
@@ -7,6 +10,32 @@ client.on('ready', () => {
     client.user.setActivity("staring into void of existence.", { type: "PLAYING"});
 });
 
-console.log('Called');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get('/', function(req, res){
+   res.sendFile(window.location.pathname + "/index.html");
+});
+
+app.post('/send', (req, res) => {
+    const embed = new MessageEmbed()
+                    .setColor("#0000ff")
+                    .setTimestamp()
+                    .setAuthor("You've got a new message!")
+                    .setDescription(stripIndents`**From:** ${req.body.name}
+                    **Email** ${req.body.email}
+                    **Message:** ${req.body.message}`);
+    client.users.cache.get(site_owner).send(embed);
+    res.redirect('/'+formPage);
+});
+
+const port = 5500;
+
+app.listen(port, () => {
+    console.log(`Server running on port${port}`);
+});
 
 client.login(token);
