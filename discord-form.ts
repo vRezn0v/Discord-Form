2599c59bacd05f30703074a33359101bcb8e62b6
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-09-19 12:42:13 +0530

    Update discord-form.ts

--- discord-form.ts
+++ discord-form.ts
@@ -2,9 +2,11 @@ const Discord = require('discord.js');
 const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 
-const { site_owner, token, formPage } = require('./config.json');
+const { site_owner, token, buttonId, formPage } = require('./config.json');
 
 const client = new Discord.Client();
+
+const PORT = process.env.PORT || 5000;
 
 client.on('ready', () => {
     client.user.setActivity("staring into void of existence.", { type: "PLAYING"});
@@ -14,38 +16,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-    if('OPTIONS' === req.method) {
-        res.sendStatus(200);
-    } else {
-        console.log(`${req.ip} ${req.method} ${req.url}`);
-        next();
-    }
-})
-
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', function(req, res){
-    res.sendFile(window.location.pathname + "/");
-});
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.post('/send', (req, res) => {
     const embed = new MessageEmbed()
                     .setColor("#0000ff")
                     .setTimestamp()
                     .setAuthor("You've got a new message!")
-                    .setDescription(stripIndents`**From:** \`\`\`${req.body.name}\`\`\`
-                    **Email** \`\`\`${req.body.email}\`\`\`
-                    **Message:** \`\`\`${req.body.message}\`\`\``);
+                    .setDescription(stripIndents`**From:** ${req.body.name}
+                    **Email** ${req.body.email}
+                    **Message:** ${req.body.message}`);
     client.users.cache.get(site_owner).send(embed);
-    res.redirect("/"+formPage);
+    res.redirect(formPage);
 });
 
-app.listen(4200, () => {
-    console.log(`Server running on port 4200`);
+app.listen(PORT, () => {
+    console.log(`Server running on port${PORT}`);
 });
 
 client.login(token);
